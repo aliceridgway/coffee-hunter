6283c70f63e00fef93dbcb16859a4bd3363cefc8
Alice Campkin <alice.l.ridgway@gmail.com>
2022-11-13 17:11:11 +0000

    register cafe models

--- backend/cafes/admin.py
+++ backend/cafes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.Cafe)
+admin.site.register(models.Review)
+admin.site.register(models.Tag)
