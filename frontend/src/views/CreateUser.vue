d2f694b1b073d67d1fc1ab3c4de9bec7832f775a
Alice Campkin <alice.l.ridgway@gmail.com>
2022-11-28 12:02:09 +0000

    add user creation form

---
+++ frontend/src/views/CreateUser.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from "vue";
+
+const username = ref(null);
+const usernameRules = ref({
+  required: (value) => !!value || "Required.",
+  min: (v) => v.length < 6 || "Min 6 characters",
+});
+
+const password = ref(null);
+const confirmPassword = ref(null);
+const passwordRules = ref({
+  required: (value) => !!value || "Required.",
+  min: (v) => v.length >= 8 || "Min 8 characters",
+});
+</script>
+
+<template>
+  <v-form ref="form">
+    <v-text-field
+      v-model="username"
+      label="Username"
+      :rules="[usernameRules.required, usernameRules.min]"
+    ></v-text-field>
+    <v-text-field
+      v-model="password"
+      label="Password"
+      :type="'password'"
+      :rules="[passwordRules.required, passwordRules.min]"
+    ></v-text-field>
+    <v-text-field
+      v-model="confirmPassword"
+      label="Confirm Password"
+      :type="'password'"
+      :rules="[passwordRules.required, passwordRules.min]"
+    ></v-text-field>
+
+    <v-btn type="submit">Register</v-btn>
+  </v-form>
+</template>
